f5d3de493d08f80c326aa23334a080c8e139c26a
Karumaidoi <mainahmwangi12@gmail.com>
2023-08-27 22:21:46 +0300

    Adding methods to array prototypes

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ Person.prototype.species = "Homo sapiens";
 
 console.log(jonas.sayHi("Alex", "Maina"));
 alex.sayHi();
+
+console.log(alex.hasOwnProperty("firstName"));
+console.log(alex.__proto__.__proto__);
+console.log(Object.prototype);
+console.log(Array.prototype);
+
+///////////////////////////////////////////
+// Adding Methods to arr prototypes
+
+const arr = [1, 2, 2, 3, 4, 2, 3, 4];
+
+Array.prototype.unique = function () {
+  console.log(this);
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
